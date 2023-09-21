24c5c19dbe0bc7fe8eb3bafa2ce74af3cf67ca6b
BigLofanbl4 <smallofanbl4@gmail.com>
2023-09-21 18:11:40 +0300

    Функция получения очков

--- script.js
+++ script.js
@@ -14,3 +14,41 @@ function getCard() {
   return getRandomArrayElement(cards);
 }
 
+function getPoint(card, currentPoints = 0) {
+  switch (card) {
+    case "2":
+      return 2;
+    case "3":
+      return 3;
+    case "4":
+      return 4;
+    case "5":
+      return 5;
+    case "6":
+      return 6;
+    case "7":
+      return 7;
+    case "8":
+      return 8;
+    case "9":
+      return 9;
+    case "T":
+    case "J":
+    case "Q":
+    case "K":
+      return 10;
+    case "A":
+      if (currentPoints + 11 > 21) {
+        return 1;
+      }
+      else  {
+        return 11;
+      }
+  }
+
+return points;
+}
+
+function blackJack() {
+  
+}
